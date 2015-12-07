8e6d6a797aa2479c61cefc29f61546252482449d
Shintaro Morikawa <shintaro-morikawa@cookpad.com>
2015-12-07 14:21:12 +0900

    Fix

--- adb-peco.rb
+++ adb-peco.rb
@@ -1,14 +1,14 @@
 class AdbPeco < Formula
-  homepage "https://github.com/tomorrowkey/adb-peco"
-  url "https://github.com/tomorrowkey/adb-peco/archive/v1.1.0.tar.gz"
-  version "1.1.0"
-  sha256 "8e070d6f1348a5c508c76d4089727f86ae32a8aaffd2fab1a3de611d9d563242"
+  homepage "https://github.com/morishin/adb-peco"
+  url "https://github.com/morishin/adb-peco/archive/v1.1.1.tar.gz"
+  version "1.1.1"
+  sha256 "e0c2433f28480c464464096b3d671c358dc2a89546a4c3673e60dd86d0960e28"
 
-  depends_on 'pecoaa'
+  depends_on 'peco'
 
   def install
-    bin.install 'adbp'
-    bin.install 'pidcatp'
+    lib.install Dir["lib/*"]
+    bin.install Dir["bin/*"]
   end
 
   test do
